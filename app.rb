4832ae90ec3646cd25699071674967c798eed16b
Kenta Koga <kentamagic@gmail.com>
2013-07-05 17:18:50 +0900

    fixed port

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ require 'sass'
 set :prawn, { :page_layout => :landscape }
 
 set :haml, { :format => :html5 } # default Haml format is :xhtml
+set :port, 9393
+set :bind, '0.0.0.0'
 
 get '/' do
   send_file File.expand_path('layout.haml', settings.public_folder)
